ff12d0ef2bd1d4bd3b57c7b375e9fa28a4518c3a
Ahmet Onur  Başterzi <ahmet@onurbasterzi.com>
2021-11-18 13:30:22 +0300

    minor updates

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,23 @@ import "../styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import { Client } from "./api/credentials";
 import Layout from "../components/layout/layout";
+import Head from 'next/head'
+import { Fragment } from "react";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Fragment>
+    <Head>
+      <title>User List Demo V2</title>
+      <meta name='keywords' content='users, list, 90Pixel React Code Challange'/>
+    </Head>
     <ApolloProvider client={Client}>
       <Layout>
+      
         <Component {...pageProps} />
       </Layout>
     </ApolloProvider>
+    </Fragment>
   );
 }
 
