7af32231f13599a16878e029d812275405deeede
Ahmet Onur  Başterzi <ahmet@onurbasterzi.com>
2021-11-18 13:45:08 +0300

    adding head and meta

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,6 @@ function MyApp({ Component, pageProps }) {
     </Head>
     <ApolloProvider client={Client}>
       <Layout>
-      
         <Component {...pageProps} />
       </Layout>
     </ApolloProvider>
